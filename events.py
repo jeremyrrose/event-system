d44a85d0ef11abedff7538c644f6922bb25c1ee4
Jeremy Rose <jeremy.r.rose@gmail.com>
2021-02-12 19:12:21 -0500

    modularized object

--- events.py
+++ events.py
@@ -1,5 +1,6 @@
 from random import random
 import pandas as pd
+from events.event import Event
 
 class Paladin:
     def __init__(self, name, damage, magic_power, attack_speed, spell_speed):
@@ -69,50 +70,6 @@ class Paladin:
 
     def __repr__(self):
         return f"{self.name} the {self.__class__.__name__}"
-
-
-class Event:
-
-    """
-    Creates an Event.
-
-    Arguments:
-        actor (any object): The object performing the event.
-        time (int): Sequential time value.
-
-    Keyword Arguments:
-        actor_events (boolean): Toggles appending event to actor.events (if actor has events attribute).
-        callbacks (list): Functions to be invoked (with current event instance as argument) at conclusion of __init__.
-    """
-
-    # class variable containing all events by any actor
-    events = []
-
-    def __init__(self, actor, time, **kwargs):
-        self.__time = time
-        self.__actor = actor
-        self.events.append(self)
-        if kwargs.get('actor_events') and hasattr(self.__actor, 'events') and isinstance(self.__actor.events, list):
-            self.__actor.events.append(self)
-        self.__callbacks = kwargs.get('callbacks') or ()
-
-    def callbacks(self):
-        for callback in self.__callbacks:
-            try:
-                callback(self)
-            except:
-                print(f"{callback} could not be executed.")
-
-    def __repr__(self):
-        return f"{self.__class__.__name__} by {self.actor} at time {self.time}"
-
-    @property
-    def time(self):
-        return self.__time
-
-    @property
-    def actor(self):
-        return self.__actor
 
 
 class Attack(Event):
---
+++ events/event.py
@@ -0,0 +1,42 @@
+class Event:
+
+    """
+    Creates an Event.
+
+    Arguments:
+        actor (any object): The object performing the event.
+        time (int): Sequential time value.
+
+    Keyword Arguments:
+        actor_events (boolean): Toggles appending event to actor.events (if actor has events attribute).
+        callbacks (list): Functions to be invoked (with current event instance as argument) at conclusion of __init__.
+    """
+
+    # class variable containing all events by any actor
+    events = []
+
+    def __init__(self, actor, time, **kwargs):
+        self.__time = time
+        self.__actor = actor
+        self.events.append(self)
+        if kwargs.get('actor_events') and hasattr(self.__actor, 'events') and isinstance(self.__actor.events, list):
+            self.__actor.events.append(self)
+        self.__callbacks = kwargs.get('callbacks') or ()
+
+    def callbacks(self):
+        for callback in self.__callbacks:
+            try:
+                callback(self)
+            except:
+                print(f"{callback} could not be executed.")
+
+    def __repr__(self):
+        return f"{self.__class__.__name__} by {self.actor} at time {self.time}"
+
+    @property
+    def time(self):
+        return self.__time
+
+    @property
+    def actor(self):
+        return self.__actor
